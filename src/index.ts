a081f79a558d1eefa00eb1043485c093b8f55ad5
nsanzimfura-eric <erickykress1@gmail.com>
2024-04-16 23:06:49 +0200

    Launch index.html on the home page to view images

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,37 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import testCreatingTreeImages from "./helpers/testData";
+import path from "path";
+import fs from "fs";
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/trees", express.static(path.join(__dirname, "trees")));
 
 const port = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//home page is just the html to view the images
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+//get images
+app.get("/images", (req: Request, res: Response) => {
+  const treesDir = path.join(__dirname, "trees");
+
+  fs.readdir(treesDir, (err, files) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error reading images directory.");
+      return;
+    }
+
+    const images = files.map((file) => ({
+      name: file,
+      url: `/trees/${file}`,
+    }));
+
+    res.status(200).json({ data: images });
+  });
 });
 
 app.listen(port, () => {
